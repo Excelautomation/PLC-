4694092527ea128cb25bb9f9eb0dc58fa0818b47
Søren Aksel Helbo Bjergmark <sahb1239@gmail.com>
2015-03-15 17:41:58 +0100

    Added sample program

--- PLC++/src/dk/aau/sw402F15/Main.java
+++ PLC++/src/dk/aau/sw402F15/Main.java
@@ -1,8 +1,46 @@
 package dk.aau.sw402F15;
 
+import dk.aau.sw402F15.parser.analysis.DepthFirstAdapter;
+import dk.aau.sw402F15.parser.lexer.Lexer;
+import dk.aau.sw402F15.parser.lexer.LexerException;
+import dk.aau.sw402F15.parser.node.Start;
+import dk.aau.sw402F15.parser.parser.Parser;
+import dk.aau.sw402F15.parser.parser.ParserException;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String code =
+                                "bool a = true && false;" +
+                                "bool b = false || true;" +
+                                "bool c = false;" +
+                                "bool d = false == false;" +
+                                "bool e = true == false;" +
+                                "bool f = true != false;" +
+                                "bool g = true != true;";
+
+        System.out.println(code);
+
+        try {
+            Parser parser = new Parser(new Lexer(new PushbackReader(new StringReader(code), 1024)));
+            Start tree = parser.parse();
+
+            // Print tree
+            tree.apply(new Printer());
+
+        } catch (ParserException e) {
+            e.printStackTrace();
+        } catch (LexerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
+
---
+++ PLC++/src/dk/aau/sw402F15/Printer.java
@@ -0,0 +1,75 @@
+package dk.aau.sw402F15;
+
+import dk.aau.sw402F15.parser.analysis.DepthFirstAdapter;
+import dk.aau.sw402F15.parser.node.*;
+
+import java.util.Stack;
+
+public class Printer extends DepthFirstAdapter {
+    Stack<Boolean> stack = new Stack<Boolean>();
+
+    @Override
+    public void outAEqualmentDeclarationDeclarationStatement(AEqualmentDeclarationDeclarationStatement node) {
+        super.outAEqualmentDeclarationDeclarationStatement(node);
+
+        System.out.println("Output: " + stack.pop());
+    }
+
+    @Override
+    public void outATrueLogicValue(ATrueLogicValue node) {
+        super.outATrueLogicValue(node);
+
+        stack.push(true);
+        System.out.println("True");
+    }
+
+    @Override
+    public void outAFalseLogicValue(AFalseLogicValue node) {
+        super.outAFalseLogicValue(node);
+
+        stack.push(false);
+        System.out.println("False");
+    }
+
+    @Override
+    public void outACompareAndLogicExpr(ACompareAndLogicExpr node) {
+        super.outACompareAndLogicExpr(node);
+
+        System.out.println("AND");
+
+        // Remember short circuit
+        boolean arg1 = stack.pop(), arg2 = stack.pop();
+        stack.push(arg1 && arg2);
+    }
+
+    @Override
+    public void outACompareOrLogicExpr(ACompareOrLogicExpr node) {
+        super.outACompareOrLogicExpr(node);
+
+        System.out.println("OR");
+
+        // Remember short circuit
+        boolean arg1 = stack.pop(), arg2 = stack.pop();
+        stack.push(arg1 || arg2);
+    }
+
+    @Override
+    public void outACompareEqualLogicTerm(ACompareEqualLogicTerm node) {
+        super.outACompareEqualLogicTerm(node);
+
+        System.out.println("Equal");
+
+        boolean arg1 = stack.pop(), arg2 = stack.pop();
+        stack.push(arg1 == arg2);
+    }
+
+    @Override
+    public void outACompareNotEqualLogicTerm(ACompareNotEqualLogicTerm node) {
+        super.outACompareNotEqualLogicTerm(node);
+
+        System.out.println("Not equal");
+
+        boolean arg1 = stack.pop(), arg2 = stack.pop();
+        stack.push(arg1 != arg2);
+    }
+}
