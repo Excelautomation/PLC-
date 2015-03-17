1e7f5bccff47c29e222f054f8bac355b627f0a0a
Søren Bjergmark <sahb1239@gmail.com>
2015-03-17 14:05:37 +0100

    Changed type byte to char

--- PLC++/src/dk/aau/sw402F15/tests/declaration/ByteDeclaration.java
+++
@@ -1,7 +0,0 @@
-package dk.aau.sw402F15.tests.declaration;
-
-public class ByteDeclaration extends Declaration {
-    public ByteDeclaration() {
-        super("byte");
-    }
-}
---
+++ PLC++/src/dk/aau/sw402F15/tests/declaration/CharDeclaration.java
@@ -0,0 +1,7 @@
+package dk.aau.sw402F15.tests.declaration;
+
+public class CharDeclaration extends Declaration {
+    public CharDeclaration() {
+        super("char");
+    }
+}
