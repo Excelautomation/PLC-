2e5dfa48271b1de7f6f6ae8562d0298442eb9054
Søren Bjergmark <sahb1239@gmail.com>
2015-04-08 13:49:13 +0200

    Added pretty printer

--- PLC++/src/dk/aau/sw402F15/Main.java
+++ PLC++/src/dk/aau/sw402F15/Main.java
@@ -54,6 +54,7 @@ public class Main {
             Start tree = parser.parse();
 
             // Print tree
+            tree.apply(new PrettyPrinter());
             tree.apply(new TypeChecker());
             tree.apply(new ExpressionEvaluator());
 
