ccc1bb12a368bfba4db7d3dfa2c144c518ad754f
Mikkel Christensen <mikkel@mclc.dk>
2015-04-08 14:03:25 +0200

    Rename

--- PLC++/src/dk/aau/sw402F15/TypeChecker/Exceptions/illegalAssignment.java
+++ PLC++/src/dk/aau/sw402F15/TypeChecker/Exceptions/illegalAssignment.java
@@ -3,5 +3,5 @@ package dk.aau.sw402F15.TypeChecker.Exceptions;
 /**
  * Created by mads on 08/04/15.
  */
-public class illegalAssignment extends RuntimeException {
+public class IllegalAssignment extends RuntimeException {
 }
