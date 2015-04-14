79cc12b58b51f8ed2c0c9c7686d4570849b66694
Søren Bjergmark <sahb1239@gmail.com>
2015-04-14 11:58:06 +0200

    More for tests

--- PLC++/src/dk/aau/sw402F15/tests/scanner/ForIteration.java
+++ PLC++/src/dk/aau/sw402F15/tests/scanner/ForIteration.java
@@ -15,4 +15,14 @@ public class ForIteration extends ScannerTest {
     public void forIteration() throws ParserException, IOException, LexerException {
         checkCode("void run() { for (i = 0; i < 10; i++) { } }");
     }
+
+    @Test
+    public void forIterationWithoutParams() throws ParserException, IOException, LexerException {
+        checkCode("void run() { for (;;) { } }");
+    }
+
+    @Test(expected = ParserException.class)
+    public void forIterationInRoot() throws ParserException, IOException, LexerException {
+        checkCode("for (;;) { }");
+    }
 }
