185cd25226515f315b83ba2bbbe89866f54a44f7
Mikkel Christensen <mikkel@mclc.dk>
2015-04-23 09:21:44 +0200

    Yay, ASTSimplify getting closer to done.

--- PLC++/src/dk/aau/sw402F15/ASTSimplify.java
+++ PLC++/src/dk/aau/sw402F15/ASTSimplify.java
@@ -15,17 +15,13 @@ public class ASTSimplify extends DepthFirstAdapter {
     @Override
     public void outAForStatement(AForStatement node) {
         List<PStatement> statementList = new ArrayList<PStatement>();
+        List<PStatement> statements = new ArrayList<PStatement>();
 
-        Node parent = node.parent();
+        statements.add(node.getStatement());
+        statements.add(new AExprStatement(node.getIterator()));
 
         statementList.add(new AExprStatement(node.getInitilizer()));
-        statementList.add(new AWhileStatement(node.getCondition(), node.getStatement()));
-        statementList.add(new AExprStatement(node.getIterator())); // TODO Need to add iterator to the previous while-statement
-
-        /*statementList.add(new AExprStatement(node.getIterator()));
-        statementList.add(new AExprStatement(node.getCondition()));*/
-
-        statementList.isEmpty();
+        statementList.add(new AWhileStatement(node.getCondition(), new AScopeStatement(statements)));
 
         super.outAForStatement(node);
     }
