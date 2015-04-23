57b441e7e3e4f47b19dcbafca123f8d463121284
Mikkel Christensen <mikkel@mclc.dk>
2015-04-23 13:29:34 +0200

    Removing useless stuff

--- PLC++/src/dk/aau/sw402F15/ASTSimplify.java
+++ PLC++/src/dk/aau/sw402F15/ASTSimplify.java
@@ -28,38 +28,4 @@ public class ASTSimplify extends DepthFirstAdapter {
         super.outAForStatement(node);
     }
 
-    // TODO This does not really do anything. Just a backup.
-    @Override
-    public void outASwitchStatement(ASwitchStatement node) {
-        List<PStatement> statementList = new ArrayList<PStatement>();
-
-       // Checking if the switch-case contains default
-        if (node.getLabels().getLast() instanceof ADefaultStatement)
-        {
-            /*PStatement firstStatement = node.getLabels().remove();
-            PStatement defaultStatement = node.getLabels().removeLast();
-            AScopeStatement newScope = new AScopeStatement();
-
-            for (PStatement item : node.getLabels())
-            {
-                statementList.add(new AIfStatement(new ACompareAndExpr(node.getCondition(), ((ACaseStatement)item).getCase()), ((ACaseStatement)item).getStatement()));
-            }*/
-
-            /*AIfElseStatement lulz = new AIfElseStatement(node.getCondition(), new AScopeStatement(statementList), defaultStatement);
-            int i = 8;
-            AIfElseStatement lulz = new AIfElseStatement(new ACompareEqualExpr(node.getCondition(), ((ACaseStatement)firstStatement).getCase()), new AScopeStatement(statementList),  ((ACaseStatement) node.getLabels().getLast()).getStatement());
-            node.replaceBy(lulz);*/
-
-            //AIfElseStatement lulz = new AIfElseStatement(new ACompareEqualExpr(node.getCondition(), ((ACaseStatement)node.getLabels().getFirst()).getCase()), ((ACaseStatement)node.getLabels().getFirst()).getStatement(), ((ACaseStatement)node.getLabels().get(1)).getStatement());
-            int i = 3;
-        }
-        else
-        {
-
-        }
-
-
-
-        super.outASwitchStatement(node);
-    }
 }
