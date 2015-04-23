33caf63c54434425c84e4ae0bd233741ec504160
Mikkel Christensen <mikkel@mclc.dk>
2015-04-23 08:57:21 +0200

    Added myprogram.ppp

--- PLC++/src/dk/aau/sw402F15/ASTSimplify.java
+++ PLC++/src/dk/aau/sw402F15/ASTSimplify.java
@@ -1,11 +1,10 @@
 package dk.aau.sw402F15;
 
 import dk.aau.sw402F15.parser.analysis.DepthFirstAdapter;
-import dk.aau.sw402F15.parser.node.AExprStatement;
-import dk.aau.sw402F15.parser.node.AForStatement;
-import dk.aau.sw402F15.parser.node.PStatement;
+import dk.aau.sw402F15.parser.node.*;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,7 +16,16 @@ public class ASTSimplify extends DepthFirstAdapter {
     public void outAForStatement(AForStatement node) {
         List<PStatement> statementList = new ArrayList<PStatement>();
 
+        Node parent = node.parent();
+
         statementList.add(new AExprStatement(node.getInitilizer()));
+        statementList.add(new AWhileStatement(node.getCondition(), node.getStatement()));
+        statementList.add(new AExprStatement(node.getIterator())); // TODO Need to add iterator to the previous while-statement
+
+        /*statementList.add(new AExprStatement(node.getIterator()));
+        statementList.add(new AExprStatement(node.getCondition()));*/
+
+        statementList.isEmpty();
 
         super.outAForStatement(node);
     }
