337722c7d52430333e433af030f434562b156085
Claus Wiingreen <cwiing13@student.aau.dk>
2015-04-24 14:06:30 +0200

    Added some cases to code generator

--- PLC++/src/dk/aau/sw402F15/CodeGenerator/CodeGenerator.java
+++ PLC++/src/dk/aau/sw402F15/CodeGenerator/CodeGenerator.java
@@ -8,6 +8,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  * Created by Claus on 24-04-2015.
  */
 public class CodeGenerator extends DepthFirstAdapter {
+    int jumpLabel = 0;
 
     @Override
     public void caseAAddExpr(AAddExpr node){
@@ -22,6 +23,75 @@ public class CodeGenerator extends DepthFirstAdapter {
     @Override
     public void caseAArrayExpr(AArrayExpr node){
         throw new NotImplementedException();
+    }
+
+    @Override
+    public void caseAAssignmentDeclaration(AAssignmentDeclaration node){
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void caseAIfElseStatement(AIfElseStatement node){
+        int ifLabel = getNextJump();
+        int elseLabel = getNextJump();
+
+        node.getCondition().apply(this);
+        Emit("CJP " + ifLabel);
+        node.getElse().apply(this);
+        Emit("JMP " + elseLabel);
+        Emit("JME " + ifLabel);
+        node.getStatement().apply(this);
+        Emit("JME " + elseLabel);
+    }
+
+    @Override
+    public void caseAIfStatement(AIfStatement node){
+        int label = getNextJump();
+
+        node.getCondition().apply(this);
+        Emit("CJPN " + label);
+        node.getStatement().apply(this);
+        Emit("JME " + label);
+    }
+
+    @Override
+    public void caseAForStatement(AForStatement node){
+        int jumpLabel = getNextJump();
+        int loopLabel = getNextJump();
+
+        node.getInitilizer().apply(this);
+        Emit("JMP " + jumpLabel);
+        Emit("JME " + loopLabel);
+        node.getStatement().apply(this);
+        node.getIterator().apply(this);
+        Emit("JME " + jumpLabel);
+        node.getCondition().apply(this);
+        Emit("CJP " + loopLabel);
+    }
+
+    @Override
+    public void caseASwitchStatement(ASwitchStatement node){
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void caseAWhileStatement(AWhileStatement node){
+        int jumpLabel = getNextJump();
+        int loopLabel = getNextJump();
+
+        Emit("JMP " + jumpLabel);
+        Emit("JME " + loopLabel);
+        node.getStatement().apply(this);
+        Emit("JME " + jumpLabel);
+        node.getCondition().apply(this);
+        Emit("CJP " + loopLabel);
+    }
+
+    private int getNextJump(){
+        jumpLabel = jumpLabel + 1;
+        if(jumpLabel > 255)
+            throw new IndexOutOfBoundsException();
+        return jumpLabel;
     }
 
     private void Emit(String string){
