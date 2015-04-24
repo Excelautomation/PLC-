5e96286dd4506f4f701b2b3c103a6f2d9d57e490
Søren Aksel Helbo Bjergmark <sahb1239@gmail.com>
2015-04-24 22:14:49 +0200

    Fixed code generator to use new grammar

--- PLC++/src/dk/aau/sw402F15/CodeGenerator/CodeGenerator.java
+++ PLC++/src/dk/aau/sw402F15/CodeGenerator/CodeGenerator.java
@@ -26,32 +26,41 @@ public class CodeGenerator extends DepthFirstAdapter {
     }
 
     @Override
-    public void caseAAssignmentDeclaration(AAssignmentDeclaration node){
-        throw new NotImplementedException();
+    public void caseADeclaration(ADeclaration node) {
+        super.caseADeclaration(node);
+
+        // AssignmentDeclaration
+        if (node.getExpr() != null) {
+            throw new NotImplementedException();
+        }
     }
 
     @Override
-    public void caseAIfElseStatement(AIfElseStatement node){
-        int ifLabel = getNextJump();
-        int elseLabel = getNextJump();
+    public void caseABranchStatement(ABranchStatement node) {
+        super.caseABranchStatement(node);
 
-        node.getCondition().apply(this);
-        Emit("CJP " + ifLabel);
-        node.getElse().apply(this);
-        Emit("JMP " + elseLabel);
-        Emit("JME " + ifLabel);
-        node.getStatement().apply(this);
-        Emit("JME " + elseLabel);
-    }
+        if (node.getRight() != null) {
+            // If - else statement
+            int ifLabel = getNextJump();
+            int elseLabel = getNextJump();
 
-    @Override
-    public void caseAIfStatement(AIfStatement node){
-        int label = getNextJump();
+            node.getCondition().apply(this);
+            Emit("CJP " + ifLabel);
+            node.getRight().apply(this);
+            Emit("JMP " + elseLabel);
+            Emit("JME " + ifLabel);
+            node.getLeft().apply(this);
+            Emit("JME " + elseLabel);
+        }
+        else {
+            // If statement
+            int label = getNextJump();
 
-        node.getCondition().apply(this);
-        Emit("CJPN " + label);
-        node.getStatement().apply(this);
-        Emit("JME " + label);
+            node.getCondition().apply(this);
+            Emit("CJPN " + label);
+            node.getLeft().apply(this);
+            Emit("JME " + label);
+        }
     }
 
     @Override
