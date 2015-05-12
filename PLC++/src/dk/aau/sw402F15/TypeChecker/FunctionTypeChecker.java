9e3ce3720467101a2630c46afae2f6c381b52797
Søren Bjergmark <sahb1239@gmail.com>
2015-05-12 09:22:03 +0200

    Function needs to return at all code paths

--- PLC++/src/dk/aau/sw402F15/TypeChecker/FunctionTypeChecker.java
+++ PLC++/src/dk/aau/sw402F15/TypeChecker/FunctionTypeChecker.java
@@ -15,6 +15,10 @@ public class FunctionTypeChecker extends ScopeDepthFirstAdapter {
 
     private boolean returnStatementFound = false;
 
+    public boolean returnStatementFound() {
+        return returnStatementFound;
+    }
+
     public FunctionTypeChecker(Scope rootScope, Scope currentScope, boolean hasReturnType, SymbolType returnType) {
         super(rootScope, currentScope);
 
@@ -51,7 +55,25 @@ public class FunctionTypeChecker extends ScopeDepthFirstAdapter {
             }
         }
 
-        super.caseABranchStatement(node);
+        if(node.getCondition() != null)
+        {
+            node.getCondition().apply(this);
+        }
+
+        FunctionTypeChecker functionTypeChecker = new FunctionTypeChecker(rootScope, currentScope, hasReturnType, returnType),
+                            functionTypeChecker1 = new FunctionTypeChecker(rootScope, currentScope, hasReturnType, returnType);
+        if(node.getLeft() != null)
+        {
+            node.getLeft().apply(functionTypeChecker);
+        }
+        if(node.getRight() != null)
+        {
+            node.getRight().apply(functionTypeChecker1);
+        }
+
+        if (functionTypeChecker.returnStatementFound() && functionTypeChecker1.returnStatementFound()) {
+            returnStatementFound = true;
+        }
     }
 
     @Override
